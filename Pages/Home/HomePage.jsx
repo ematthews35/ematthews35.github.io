c7cad1ff33d2c8b7a75f6e27388a9991306abd55
Eric M <matthews.eric02@gmail.com>
2020-11-26 22:38:04 -0500

    Website Infrastructure

---
+++ Pages/Home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./HomePage.css";
+
+const HomePage = () => (
+    <div className="homePage">
+        <title>Eric Matthews</title>
+
+        <div className="Biographical Information">
+            <h2>Here's some information about me~</h2>
+        </div>
+    </div>
+
+)
